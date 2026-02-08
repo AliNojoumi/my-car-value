8ce695b32914d9cd0f55d8cf77cac5e6d92f364b
AliNojoumi <anojomi3@gmail.com>
2026-02-08 18:27:47 +0330

    commited project up to interceptors cause of INT issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '500Ali9604',
+    database: 'nestjs_db',
+    // entities: [User, Report],
+    autoLoadEntities: true,
+    synchronize: true,
+  }), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
