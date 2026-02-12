63c7916568e796d8aaa3a77c9e95f21f157a3164
AliNojoumi <anojomi3@gmail.com>
2026-02-12 08:19:42 +0330

    Added cookie-session to the signup and signin and added signout

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -25,6 +25,11 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
+  }
+
   //This is test example of sessions
   // @Get('/colors/:color')
   // setColor(@Param('color') color: string, @Session() session: any) {
@@ -36,14 +41,23 @@ export class UsersController {
   //   return session.color;
   // }
 
+  @Post('/signout')
+  signOut(@Session() session:any){
+    session.userId = null;
+  }
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Get('/:id')
